7064455466a437c8c877f011d4a03d1dac383e5c
Fine <fine@FinedeMacBook-Air.local>
2024-08-25 15:37:07 +0800

    Initial commit

---
+++ UIE/InstructUIE-master/scripts/eval_flan-t5.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -x
+
+export CUDA_DEVICE_ORDER="PCI_BUS_ID"
+export TRANSFORMERS_CACHE=/root/.cache/huggingface
+
+port=$(shuf -i25000-30000 -n1)
+
+# 最好令 bs_per_gpu * num_gpu * gradient_accumulation_steps = 256
+# 学习率可以使用 5e-5
+# param_num < 1b 10epoch, 3b 5epoch, 11b 5epoch
+# 注意修改 CUDA_VISIBLE_DEVICES, model_name_or_path，output_dir, run_name, data_dir, task_config_dir, instruction_file
+# 其余参数可与当前版本保持一致
+
+# 3090 * 4 on t5-700M
+CUDA_VISIBLE_DEVICES=0
+deepspeed src/run_uie.py \
+   --do_predict \
+   --predict_with_generate \
+   --model_name_or_path /Users/fine/Desktop/flan-t5-small \
+   --data_dir /Users/fine/Downloads/cjtl1.txt \
+   --task_config_dir /Users/fine/PycharmProjects/pythonProject/UIE/InstructUIE-master/configs/zeroshot_configs/test_tasks.json \
+   --instruction_file /Users/fine/Desktop/InstructUIE-master/configs/instruction_config.json \
+   --instruction_strategy single \
+   --input_record_file flan-t5.record \
+   --per_device_eval_batch_size 16 \
+   --deepspeed configs/ds_configs/stage0.config \
+   --run_name t5-700M-mult-mi-experiment \
+   --max_source_length 512 \
+   --max_target_length 50 \
+   --generation_max_length 50 \
+   --max_num_instances_per_eval_task 200 \
+   --add_task_name False \
+   --add_dataset_name False \
+   --num_examples 0 \
+   --overwrite_output_dir \
+   --overwrite_cache
